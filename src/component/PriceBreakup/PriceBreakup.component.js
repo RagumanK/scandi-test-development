95609b7fb218453201e6b1eac3ea2e03bd9206b7
RagumanK <raguman@emproto.com>
2022-03-09 18:32:58 +0530

    Config Done

--- src/component/PriceBreakup/PriceBreakup.component.js
+++ src/component/PriceBreakup/PriceBreakup.component.js
@@ -4,6 +4,7 @@
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
+import Loader from 'Component/Loader/Loader.component';
 import { AttributeType } from 'Type/ProductList.type';
 
 import './PriceBreakup.style';
@@ -16,47 +17,52 @@ export class PriceBreakupComponent extends PureComponent {
       attributesWithValues: AttributeType.isRequired,
       priceBreakupValues: PropTypes.isRequired,
       load: PropTypes.isRequired,
+      loadConfig: PropTypes.isRequired,
       productType: PropTypes.isRequired
-
   };
 
   // eslint-disable-next-line lines-between-class-members
   renderDetails(data) {
+      console.log('dataaa', data);
       return data.map((group) => (
-          <tr>
-          <td>{ group.name }</td>
-          <td>{ group.rate }</td>
-          <td>{ group.weight }</td>
-          <td>{ group.value }</td>
-          <td>{ group.discount }</td>
-          <td>{ group.fval }</td>
-          </tr>
+      <tr>
+        <td>{ group.name }</td>
+        <td>{ group.rate }</td>
+        <td>{ group.weight }</td>
+        <td>{ group.value }</td>
+        <td>{ group.discount }</td>
+        <td>{ group.fval }</td>
+      </tr>
       ));
   }
 
   renderDetailsBold(data) {
       return data.map((group) => (
-          <tr>
-          <td><b>{ group.name }</b></td>
-          <td>{ group.rate }</td>
-          <td>{ group.weight }</td>
-          <td>{ group.value }</td>
-          <td>{ group.discount }</td>
-          <td>{ group.fval }</td>
-          </tr>
+      <tr>
+        <td>
+          <b>{ group.name }</b>
+        </td>
+        <td>{ group.rate }</td>
+        <td>{ group.weight }</td>
+        <td>{ group.value }</td>
+        <td>{ group.discount }</td>
+        <td>{ group.fval }</td>
+      </tr>
       ));
   }
 
   renderPriceDetails(label, value) {
       return (
-        <tr>
-          <td><b>{ label }</b></td>
-          <td />
-          <td />
-          <td>{ value }</td>
-          <td />
-          <td>{ value }</td>
-        </tr>
+      <tr>
+        <td>
+          <b>{ label }</b>
+        </td>
+        <td />
+        <td />
+        <td>{ value }</td>
+        <td />
+        <td>{ value }</td>
+      </tr>
       );
   }
 
@@ -67,22 +73,24 @@ export class PriceBreakupComponent extends PureComponent {
       const priceBreakupValues = this.props.priceBreakupValues[0];
       // eslint-disable-next-line react/destructuring-assignment
       return (
-          <table>
-            <thead>
-            <tr>
-              <th>Component</th>
-              <th>Rate</th>
-              <th>Approx Weight</th>
-              <th>Value</th>
-              <th>Discount</th>
-              <th>Final Value</th>
-            </tr>
-            </thead>
-            <tbody>
+      <table>
+        <thead>
+          <tr>
+            <th>Component</th>
+            <th>Rate</th>
+            <th>Approx Weight</th>
+            <th>Value</th>
+            <th>Discount</th>
+            <th>Final Value</th>
+          </tr>
+        </thead>
+        <tbody>
           { priceBreakupValues.metal.values.length > 0 ? (
               <>
               <tr>
-                <td><b>Metals</b></td>
+                <td>
+                  <b>Metals</b>
+                </td>
                 <td />
                 <td />
                 <td />
@@ -92,14 +100,15 @@ export class PriceBreakupComponent extends PureComponent {
               { this.renderDetails(priceBreakupValues.metal.values) }
               { this.renderPriceDetails('Total Metal Values', priceBreakupValues.metal.total_metal_value) }
               </>
-
           ) : (
               ''
           ) }
           { priceBreakupValues.diamond.values.length > 0 ? (
               <>
               <tr>
-                <td><b>Diamonds</b></td>
+                <td>
+                  <b>Diamonds</b>
+                </td>
                 <td />
                 <td />
                 <td />
@@ -115,7 +124,9 @@ export class PriceBreakupComponent extends PureComponent {
           { priceBreakupValues.stone.values.length > 0 ? (
               <>
               <tr>
-                <td><b>Stones</b></td>
+                <td>
+                  <b>Stones</b>
+                </td>
                 <td />
                 <td />
                 <td />
@@ -131,26 +142,27 @@ export class PriceBreakupComponent extends PureComponent {
           { this.renderDetailsBold(priceBreakupValues.making_charges.values) }
           { this.renderDetailsBold(priceBreakupValues.subtotal.values) }
           { this.renderDetailsBold(priceBreakupValues.grand_total.values) }
-            </tbody>
-          </table>
+        </tbody>
+      </table>
       );
   }
 
   renderContent2() {
-      return (
-      <h1> Hello</h1>
-      );
+      return <h4>Please Select Config</h4>;
   }
 
   render() {
-      const { load } = this.props;
-      console.log(this.props);
+      const { load, loadConfig } = this.props;
       // eslint-disable-next-line react/destructuring-assignment
       if (!load) {
-          return null;
+          return <Loader isLoading={ !load } />;
       }
       // eslint-disable-next-line react/destructuring-assignment
       if (this.props.productType === 'simple' && load) {
+          return this.renderContent();
+      }
+      // eslint-disable-next-line react/destructuring-assignment
+      if (this.props.productType === 'configurable' && loadConfig) {
           return this.renderContent();
       }
 
--- src/component/PriceBreakup/PriceBreakup.container.js
+++ src/component/PriceBreakup/PriceBreakup.container.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable dot-notation */
 /* eslint-disable fp/no-let */
 /**
@@ -30,34 +32,54 @@ export class PriceBreakupContainer extends PureComponent {
 
   componentDidMount() {
       // eslint-disable-next-line react/destructuring-assignment
-      this.restApiCall(this.props.product.id);
+      this.restApiCall(this.props.product.id, this.props.product.type_id);
   }
 
   componentDidUpdate(prevProps) {
-      // eslint-disable-next-line react/destructuring-assignment
-      console.log(this.props.product, prevProps.product);
+      if (this.props.product.attributes.sku.attribute_value !== prevProps.product.attributes.sku.attribute_value) {
+          this.getConfigPriceBreakup(this.props.product.attributes.sku.attribute_value);
+      }
   }
 
   __construct(props) {
       super.__construct(props);
-      this.state = { dataLoaded: false, data: {} };
+      this.state = { dataLoaded: false, dataLoadedConfig: false, data: {} };
   }
 
-  async restApiCall(id) {
+  getConfigPriceBreakup(id) {
+      if (this.state.configData) {
+          // eslint-disable-next-line react/no-access-state-in-setstate
+          const data = [];
+          data.push(this.state.configData[0].variants[id]);
+          this.setState({
+              data,
+              dataLoadedConfig: true
+          });
+      }
+  }
+
+  async restApiCall(id, type) {
       try {
           // eslint-disable-next-line quotes
           const response = await fetch(`https://magento.aayke.com/rest/V1/pricebreakup?param[product_id]=${id}`, {
-          // eslint-disable-next-line quotes
-          // const response = await fetch(`https://catfact.ninja/fact`, {
               method: 'GET',
               headers: { 'Content-Type': 'application/json' }
           });
           const data = await response.json();
-          this.setState({
-          // eslint-disable-next-line react/no-unused-state
-              dataLoaded: true,
-              data
-          });
+          if (type === 'simple') {
+              this.setState({
+              // eslint-disable-next-line react/no-unused-state
+                  dataLoaded: true,
+                  data
+              });
+          }
+          if (type === 'configurable') {
+              this.setState({
+                  // eslint-disable-next-line react/no-unused-state
+                  configData: data,
+                  dataLoaded: true
+              });
+          }
       } catch (error) {
           console.log(error);
           this.setState({ dataLoaded: false });
@@ -65,25 +87,6 @@ export class PriceBreakupContainer extends PureComponent {
 
       //   return data;
   }
-
-  //   generatePriceBreakupData(product) {
-  //       // eslint-disable-next-line prefer-const
-  //       let arr = {};
-  //       this.restApiCall(product.id).then(
-  //           /** @namespace myApp/Component/PriceBreakup/Container/PriceBreakupContainer/generatePriceBreakupData/restApiCall/then */
-  //           (value) => {
-  //               arr.data = value;
-  //               return arr;
-  //           }
-  //       );
-  //   }
-
-  //   generatePriceBreakupData2(product) {
-  //       // eslint-disable-next-line prefer-const
-  //       let arr = {};
-  //       arr.data = this.restApiCall(product.id);
-  //       return arr;
-  //   }
 
   containerProps() {
       const { areDetailsLoaded, product } = this.props;
@@ -94,7 +97,10 @@ export class PriceBreakupContainer extends PureComponent {
           // eslint-disable-next-line react/destructuring-assignment
           priceBreakupValues: this.state.data,
           // eslint-disable-next-line react/destructuring-assignment
-          load: this.state.dataLoaded
+          load: this.state.dataLoaded,
+          // eslint-disable-next-line react/destructuring-assignment
+          loadConfig: this.state.dataLoadedConfig
+
       };
   }
 
